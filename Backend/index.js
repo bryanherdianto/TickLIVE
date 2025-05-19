c052b6f2c46500fa4d119685115565725d08645a
Naufal Hadi Rasikhin <naufal.hdr153@gmail.com>
2025-05-19 20:58:53 +0700

    adding user, ticket, and seat more endpoint

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/user', require('./src/routes/user.routes'))
+app.use('/seat', require('./src/routes/seat.routes'))
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
