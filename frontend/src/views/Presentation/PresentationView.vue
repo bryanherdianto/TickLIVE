08e3ddf5f389c6793fad96682b266a24fc4a985d
Bryan Herdianto <bryan.herdianto17@gmail.com>
2025-05-20 15:28:45 +0700

    initial commit frontend, TODO: make details page for event and venue

---
+++ frontend/src/views/Presentation/PresentationView.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { onMounted, onUnmounted } from "vue";
+
+import NavbarDefault from "../..//examples/navbars/NavbarDefault.vue";
+import DefaultFooter from "../../examples/footers/FooterDefault.vue";
+import Header from "../../examples/Header.vue";
+import FilledInfoCard from "../../examples/cards/infoCards/FilledInfoCard.vue";
+
+import PresentationCounter from "./Sections/PresentationCounter.vue";
+import PresentationPages from "./Sections/PresentationPages.vue";
+import PresentationExample from "./Sections/PresentationExample.vue";
+import data from "./Sections/Data/designBlocksData";
+import BuiltByDevelopers from "./Components/BuiltByDevelopers.vue";
+import PresentationTestimonials from "./Sections/PresentationTestimonials.vue";
+import PresentationInformation from "./Sections/PresentationInformation.vue";
+import Contact from "./Sections/AuthorContact.vue";
+
+import vueMkHeader from "@/assets/img/vue-mk-header.jpg";
+
+const body = document.getElementsByTagName("body")[0];
+onMounted(() => {
+  body.classList.add("presentation-page");
+  body.classList.add("bg-gray-200");
+});
+onUnmounted(() => {
+  body.classList.remove("presentation-page");
+  body.classList.remove("bg-gray-200");
+});
+</script>
+
+<template>
+  <div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+      <div class="col-12">
+        <NavbarDefault :sticky="true" />
+      </div>
+    </div>
+  </div>
+  <Header>
+    <div class="page-header min-vh-75" :style="`background-image: url(${vueMkHeader})`" loading="lazy">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 text-center mx-auto position-relative">
+            <h1 class="text-white pt-3 mt-n5 me-2" :style="{ display: 'inline-block' }">
+              Discover and Create Your Perfect Event Space
+            </h1>
+            <p class="lead text-white px-5 mt-3" :style="{ fontWeight: '500' }">
+              Unparalleled luxury and comfort await at the world's most exclusive events and venues. Start your journey
+              today.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Header>
+
+  <div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n6">
+    <PresentationCounter />
+    <PresentationInformation />
+    <PresentationExample :data="data" />
+    <PresentationPages />
+    <BuiltByDevelopers />
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <FilledInfoCard class="p-4" :color="{ text: 'white', background: 'bg-gradient-success' }"
+            :icon="{ component: 'flag', color: 'white' }" title="Getting Started"
+            description="Check the possible ways of working with our product and the necessary files for building your own project."
+            :action="{
+              route:
+                'https://www.creative-tim.com/learning-lab/vue/overview/material-kit/',
+              label: { text: 'Let\'s start', color: 'white' }
+            }" />
+        </div>
+        <div class="col-lg-4">
+          <FilledInfoCard class="px-lg-1 mt-lg-0 mt-4 p-4" height="h-100"
+            :icon="{ component: 'precision_manufacturing', color: 'success' }" title="Plugins" description="Get inspiration and have an overview about the plugins that we
+                used to create the Material Kit." :action="{
+                  route:
+                    'https://www.creative-tim.com/learning-lab/vue/input/material-kit/',
+                  label: { text: 'Read more' }
+                }" />
+        </div>
+        <div class="col-lg-4">
+          <FilledInfoCard class="px-lg-1 mt-lg-0 mt-4 p-4" :icon="{ component: 'receipt_long', color: 'success' }"
+            title="Utility Classes" description="Material Kit is giving you a lot of pre-made elements. For those
+                who want flexibility, we included many utility classes." :action="{
+                  route:
+                    'https://www.creative-tim.com/learning-lab/vue/utilities/material-kit/',
+                  label: { text: 'Read more' }
+                }" />
+        </div>
+      </div>
+    </div>
+    <PresentationTestimonials />
+    <Contact />
+  </div>
+  <DefaultFooter />
+</template>
