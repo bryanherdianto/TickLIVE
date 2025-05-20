1d2add90f624c56c755a59c5f9fcd98a47989e67
Naufal Hadi Rasikhin <naufal.hdr153@gmail.com>
2025-05-20 20:40:18 +0700

    modify few functions

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,12 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use('/user', require('./src/routes/user.routes'))
 app.use('/seat', require('./src/routes/seat.routes'))
--- Backend/src/routes/location.routes.js
+++ Backend/src/routes/location.routes.js
@@ -1,11 +1,15 @@
 const locationController = require('../controllers/location.controller');
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.get('/', locationController.getAllLocations);
 router.get('/:id', locationController.getLocationById);
-router.post('/create', locationController.createLocation);
-router.put('/update/:id', locationController.updateLocation);
+router.post('/create', upload.single('image'), locationController.createLocation);
+router.put('/update/:id', upload.single('image'), locationController.updateLocation);
 router.delete('/delete/:id', locationController.deleteLocation);
 
 module.exports = router;
